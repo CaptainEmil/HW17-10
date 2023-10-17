0cc85e71480faed107e69515eab00ede5c670601
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-10-17 20:42:35 +0400

    2nd-point of HW is done

--- HW17-10/index.js
+++ HW17-10/index.js
@@ -2,7 +2,17 @@
 
 console.clear();
 
+const obj = {
+    name: 'Alex',
+    surName: 'Max',
+    AGE: 40
+}
 
-const titles = ['Заголовок 1', 'Очень длинный заголовок во всей книжке', 'Заголовок 2'];
-const pages = [10, 20, 123456789012345];
-titles.map((x,i)=>console.log(`${x.concat('.').padEnd(20-String(pages[i]).split('').length,'.')}${pages[i]}`));
+while(true){
+    let field = prompt('Enter the field').toLowerCase();
+    let isIncluded=false;
+    for(let key in obj) {
+        if(key.toLowerCase()===field)isIncluded=true;
+    }
+    console.log(isIncluded);
+}
